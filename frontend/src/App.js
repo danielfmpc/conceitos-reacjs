101bfa8f5befe937247d4a505a7b8920791a52b6
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2020-04-10 04:42:19 -0300

    conf babel e mudando estado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,28 @@ import './App.css';
 import api from './services/api.js'
 import Header from './components/Header';
 
-useEffect(()=> {
-  api.get('/projects').then(response => {
-    console.log(response);
-  });
-}, []);
+
 
 function App() {
-  const [projects, setProjects] = useState(["Dev app", "front-end"]);
+  const [projects, setProjects] = useState([]);
+  useEffect(()=> {
+    api.get('/projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
 
-  function handleAddProjects() {
-    setProjects([... projects, `Novo projeto ${Date.now()}`]);
+  async function handleAddProjects() {
+    // setProjects([... projects, `Novo projeto ${Date.now()}`]);
+
+    const response = await api.post('projects',
+      {
+        title: `Novo projeto ${Date.now()}`,
+        owner: "Daniel Fernando"
+      }
+    );
+    const project = response.data;
+
+    setProjects([... projects, project]);
   }
 
   return(
@@ -22,7 +33,7 @@ function App() {
     <Header title="Projects" />
     {/* <img src={image} width={300} alt="plano de fundo"/> */}
     <ul>
-      {projects.map(project => <li key={project}>{project}</li>)}
+      {projects.map(project => <li key={project.id}>{project.title}</li>)}
     </ul>
     <button type="button" onClick={handleAddProjects}>Adicionar projetos</button>
   </>
